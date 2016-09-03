ce658c40f24776256125a8b8ad78021cf39dcfd3
gtamoro <gtamoro@gmail.com>
2016-09-04 00:28:26 +1000

    JS for landing page

--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $("#button").click(function() {
     alert("We're Not Ready For Sign-Ups...Yet.");
   });
+
+  $(".secondary").click(function() {
+    alert("We're Not Ready For Sign-Ups...Yet.");
+  });
 });

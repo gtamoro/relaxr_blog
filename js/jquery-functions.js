a1bdbc02b370d2c6a8d097eacf6dc1b7c932b185
gtamoro <gtamoro@gmail.com>
2016-09-04 16:27:36 +1000

    Stripped out common styles into base.css

--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-  $("#button").click(function() {
-    alert("We're Not Ready For Sign-Ups...Yet.");
-  });
-
-  $(".secondary").click(function() {
+  $("#button.signup").click(function() {
     alert("We're Not Ready For Sign-Ups...Yet.");
   });
 });
